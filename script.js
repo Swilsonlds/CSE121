b3c51351c36c638278cbf12041535179a186e40d
Swilsonlds <91799590+Swilsonlds@users.noreply.github.com>
2022-05-29 02:14:14 -0600

    Done, kind of

---
+++ script.js
@@ -0,0 +1,23 @@
+const jokeHTML = document.querySelector('#joke')
+const jokeButton = document.querySelector('#GetJoke')
+// gets a new joke from the api //
+
+jokeButton.addEventListener("click", getJoke())
+
+getJoke();
+
+async function getJoke() {
+    const response = await fetch('https://icanhazdadjoke.com/', {
+		headers: {
+			'Accept': 'application/json'
+		}
+	})
+    let joke = await response.json();
+
+	jokeHTML.innerHTML = joke.joke;
+}
+
+function clear() {
+	jokeHTML.innerHTML = ''
+}
+
