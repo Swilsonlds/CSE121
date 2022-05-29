8c122079058d71753f05dee63eb14994113b2f08
Swilsonlds <91799590+Swilsonlds@users.noreply.github.com>
2022-05-29 12:38:17 -0600

    tweaks

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-const jokeHTML = document.querySelector('#joke')
+// My program is a random joke generator //
+// it will retrieve both dad jokes, and random jokes //
+// some of the normal jokes can be kind of filthy //
+// so I apologize in advance //
+
+// data needed for fetching regular joke //
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'jokeapi-v2.p.rapidapi.com',
+		'X-RapidAPI-Key': 'aa42676130msh7e7cb379e03f053p1498fajsn8d2142eefd54'
+	}
+};
+
+const jokeHTML = document.querySelector('#joke1')
+const joke2HTML = document.querySelector('#joke2')
 const jokeButton = document.querySelector('#GetJoke')
-// gets a new joke from the api //
+const jokeButton2 = document.querySelector('#GetJoke2')
+const clearButton = document.querySelector('#clear')
 
-jokeButton.addEventListener('click', getJoke)
+// button that calls function to get new joke//
+jokeButton.addEventListener('click', getDadJoke)
+jokeButton2.addEventListener('click', getNormalJoke)
 
-getJoke();
+//  calls function that clears joke that is currently displayed
+clearButton.addEventListener('click', clear)
 
-async function getJoke() {
+// retrieves dad joke from the API and stores it in the HTML //
+async function getDadJoke() {
+	jokeHTML.innerHTML = ''
+	joke2HTML.innerHTML = ''
     const response = await fetch('https://icanhazdadjoke.com/', {
 		headers: {
 			'Accept': 'application/json'
@@ -17,7 +39,20 @@ async function getJoke() {
 	jokeHTML.innerHTML = joke.joke;
 }
 
+// retrieves non-dad joke from different API and stores it in HTML //
+async function getNormalJoke() {
+	jokeHTML.innerHTML = ''
+	joke2HTML.innerHTML = ''
+    const response = await fetch('https://jokeapi-v2.p.rapidapi.com/joke/Any?format=json', options)
+	
+    let joke = await response.json();
+
+	jokeHTML.innerHTML = joke.setup;
+	joke2HTML.innerHTML = joke.delivery;
+}
+
 function clear() {
 	jokeHTML.innerHTML = ''
+	joke2HTML.innerHTML = ''
 }
 
